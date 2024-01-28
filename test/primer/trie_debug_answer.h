e8db44f2a020566ea5e9f89e86e64b5fdd1ff6c1
therehello <therehello@qq.com>
2024-01-28 10:38:56 +0800

    ✨ feat: Implement trie_debug

--- test/primer/trie_debug_answer.h
+++ test/primer/trie_debug_answer.h
@@ -1,16 +1,19 @@
 #include "common/exception.h"
 
 auto Case1CorrectAnswer() -> int {
+  return 10;
   throw bustub::NotImplementedException(
       "The correct answer is hidden. Submit to Gradescope to see if your answer is correct.");
 }
 
 auto Case2CorrectAnswer() -> int {
+  return 1;
   throw bustub::NotImplementedException(
       "The correct answer is hidden. Submit to Gradescope to see if your answer is correct.");
 }
 
 auto Case3CorrectAnswer() -> int {
+  return 25;
   throw bustub::NotImplementedException(
       "The correct answer is hidden. Submit to Gradescope to see if your answer is correct.");
 }
