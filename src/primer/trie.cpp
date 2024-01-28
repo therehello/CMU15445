170fd04ff1bb304036e84d17d891450ee21aa80d
therehello <therehello@qq.com>
2024-01-28 13:40:23 +0800

    🌈 style: Trie::Get use auto

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -8,7 +8,7 @@ namespace bustub {
 
 template <class T>
 auto Trie::Get(std::string_view key) const -> const T * {
-  std::shared_ptr<const TrieNode> curr = root_;
+  auto curr = root_;
   for (auto &ch : key) {
     try {
       curr = curr->children_.at(ch);
@@ -21,7 +21,7 @@ auto Trie::Get(std::string_view key) const -> const T * {
     return node->value_.get();
   }
   return nullptr;
-  
+
   // You should walk through the trie to find the node corresponding to the key. If the node doesn't exist, return
   // nullptr. After you find the node, you should use `dynamic_cast` to cast it to `const TrieNodeWithValue<T> *`. If
   // dynamic_cast returns `nullptr`, it means the type of the value is mismatched, and you should return nullptr.
