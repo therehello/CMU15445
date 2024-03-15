3b49ef772040926f1c2d52a0baa56905a629f2d4
therehello <therehello@qq.com>
2024-03-15 18:40:05 +0800

    ✨ feat(query execution): Implement SeqScan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,33 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include <memory>
+#include "storage/table/table_iterator.h"
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan), table_info(exec_ctx->GetCatalog()->GetTable(plan_->GetTableOid())) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() { iter_ = std::make_unique<TableIterator>(table_info->table_->MakeIterator()); }
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  for (; !iter_->IsEnd(); ++(*iter_)) {
+    auto [tuple_meta, _tuple] = iter_->GetTuple();
+    if (tuple_meta.is_deleted_) {
+      continue;
+    }
+    *tuple = _tuple;
+    *rid = iter_->GetRID();
+    if (plan_->filter_predicate_) {
+      if (!plan_->filter_predicate_->Evaluate(tuple, table_info->schema_).GetAs<bool>()) {
+        continue;
+      }
+    }
+    ++(*iter_);
+    return true;
+  }
+  return false;
+}
 
 }  // namespace bustub
